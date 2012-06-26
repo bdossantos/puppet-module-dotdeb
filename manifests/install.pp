24b0002b6e00212508c3fb250f7fb9c34e4ed95b
Benjamin Dos Santos <benj.ds@gmail.com>
2012-06-27 00:08:23 +0200

    coding style

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,35 +1,33 @@
 class dotdeb::install {
 
-    file {
-        '/etc/apt/sources.list.d':
-        ensure  => 'directory',
-        owner   => 'root',
-        group   => 'root';
+  file { '/etc/apt/sources.list.d':
+      ensure  => 'directory',
+      owner   => 'root',
+      group   => 'root';
+  }
 
-        '/etc/apt/sources.list.d/dotdeb.list':
-        ensure  => 'present',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0600',
-        content => template('dotdeb/dotdeb.list.erb')
-    }
+  file { '/etc/apt/sources.list.d/dotdeb.list':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => template('dotdeb/dotdeb.list.erb');
+  }
 
-    exec {
-        'dotdeb-apt-key':
-        path    => '/bin:/usr/bin',
-        cwd     => '/tmp',
-        command => "wget ${dotdeb::key} -O dotdeb.gpg &&
-                    cat dotdeb.gpg | apt-key add -",
-        unless  => 'apt-key list | grep dotdeb',
-        require => File['/etc/apt/sources.list.d/dotdeb.list'],
-        notify  => Exec['update-apt'];
-    }
+  exec { 'dotdeb-apt-key':
+    path    => '/bin:/usr/bin',
+    cwd     => '/tmp',
+    command => "wget ${dotdeb::key} -O dotdeb.gpg &&
+                cat dotdeb.gpg | apt-key add -",
+    unless  => 'apt-key list | grep dotdeb',
+    require => File['/etc/apt/sources.list.d/dotdeb.list'],
+    notify  => Exec['update-apt'];
+  }
 
-    exec {
-        'update-apt':
-        path        => '/bin:/usr/bin',
-        command     => 'apt-get update',
-        require     => Exec['dotdeb-apt-key'],
-        refreshonly => true;
-    }
+  exec { 'update-apt':
+    path        => '/bin:/usr/bin',
+    command     => 'apt-get update',
+    require     => Exec['dotdeb-apt-key'],
+    refreshonly => true;
+  }
 }
